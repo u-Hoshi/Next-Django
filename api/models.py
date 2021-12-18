1d41b8eb7025754762cb25b20cfae9d60ba9a71a
u-Hoshi <yuta000707@yahoo.co.jp>
2021-12-18 14:40:38 +0900

    api作成

---
+++ api/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    tags = models.ManyToManyField("Tag")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
---
+++ api/views.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets, generics
+from rest_framework.permissions import AllowAny
+from . import serializers
+from .models import Blog
+
+class CreateUserView(generics.CreateAPIView):
+    serializers_class = serializers.UserSerializer
+    permission_classes = (AllowAny,)
+
+class BlogReadOnlyView(viewsets.ReadOnlyModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = serializers.BlogSerializer
+    permission_classes = (AllowAny,)
+
+class DeleteBlogView(generics.DestroyAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = serializers.BlogSerializer
